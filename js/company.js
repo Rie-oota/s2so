0bc293ca3856ebd3fea53798bf8bb9cfb6c54e66
Yoshida <sunnri257@gmail.com>
2025-09-29 21:32:57 +0900

    保存①

---
+++ js/company.js
@@ -0,0 +1,34 @@
+// 表示領域に入ったらclass付与
+$(function () {
+  $('.inview').on("inview", function () {
+    $(this).addClass('show');
+  });
+});
+
+// トップ遷移ボタンをクリックするとトップに遷移する
+$(function() {
+  $(".top-btn").click(function(){
+    $('html,body').animate({
+      scrollTop: 0
+    });
+  });
+});
+
+$(function () {
+  // id[js-stalker]を取得する
+  const stalker = $("#js-stalker");
+  // 不透明にする
+  $(stalker).css("opacity","1");
+  // マウスを移動させたときの処理を指定する
+  $(document).on("mousemove", function (e) {
+    // マウスの座標を取得
+    const x = e.clientX;
+    const y = e.clientY;
+    // ストーカーの位置を更新する
+    $(stalker).css("transform","translate(" + x + "px, " + y + "px)");
+  });
+  // アニメーションを更新する
+  (function update(){
+    window.requestAnimationFrame(update);
+  })();
+});
